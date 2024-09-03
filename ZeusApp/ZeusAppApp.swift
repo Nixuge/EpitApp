6f86cdd402a0fba78bf8973c36833d5b965d02e8
Nixuge <mail@nixuge.me>
2024-09-03 11:20:41 +0200

    Initial Commit

---
+++ ZeusApp/ZeusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeusAppApp.swift
+//  ZeusApp
+//
+//  Created by Quenting on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZeusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
