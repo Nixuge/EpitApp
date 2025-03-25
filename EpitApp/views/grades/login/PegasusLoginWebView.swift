12b681f1948c021bc3bc29deffec2bce91c4dab8
Nixuge <mail@nixuge.me>
2025-03-25 23:01:45 +0100

    Overhauled Pegasus login to look for something on the html of the result page instead of simply looking at the cookies (unreliable), should now work pretty much perfectly.

--- EpitApp/views/grades/login/PegasusLoginWebView.swift
+++ EpitApp/views/grades/login/PegasusLoginWebView.swift
@@ -16,31 +16,59 @@ struct PegasusLoginWebView: UIViewRepresentable {
             self._isPresented = isPresented
         }
 
-        func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-            
-            
-            if navigationResponse.response.url!.absoluteString == "https://prepa-epita.helvetius.net/pegasus/index.php" {
+//        func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+//            
+//            
+//            if navigationResponse.response.url!.absoluteString == "https://prepa-epita.helvetius.net/pegasus/index.php" {
+//                WKWebsiteDataStore.default().httpCookieStore.getAllCookies() { cookies in
+//                    // 1 cookie = only contains Pegasus' original PHPSESSID, not logged in
+//                    // > 1 = more cookies from eg office login.
+//                    if (cookies.count > 1) {
+//                        for cookie in cookies {
+//                            // TODO: check for expiration using the ai_session cookie
+//                            print("\(cookie.expiresDate) - \(cookie.name)")
+//                            if (cookie.domain == "prepa-epita.helvetius.net" && cookie.name == "PHPSESSID" && cookie.expiresDate ?? Date.distantFuture < Date.now) {
+//                                print("PHPSESSID Cookie: \(cookie.value)")
+////                                decisionHandler(.cancel)
+//                                
+//                                self.pegasusAuthModel.setPhpSessId(newPhpSessId: cookie.value)
+//                                DispatchQueue.main.async {
+//                                    self.isPresented = false
+//                                }
+//                                return
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            decisionHandler(.allow)
+//        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            print("End loading")
+            webView.evaluateJavaScript("document.documentElement.outerHTML", completionHandler: { result, error in
+                guard let dataHtml = result as? String else {
+                   return
+                }
+                guard dataHtml.contains("<td class=\"logout item\">") else {
+                   return
+                }
                 WKWebsiteDataStore.default().httpCookieStore.getAllCookies() { cookies in
-                    // 1 cookie = only contains Pegasus' original PHPSESSID, not logged in
-                    // > 1 = more cookies from eg office login.
-                    if (cookies.count > 1) {
-                        for cookie in cookies {
-                            if (cookie.domain == "prepa-epita.helvetius.net" && cookie.name == "PHPSESSID") {
-                                print("PHPSESSID Cookie: \(cookie.value)")
-//                                decisionHandler(.cancel)
-                                
-                                self.pegasusAuthModel.setPhpSessId(newPhpSessId: cookie.value)
-                                DispatchQueue.main.async {
-                                    self.isPresented = false
-                                }
-                                return
+                    for cookie in cookies {
+                        if (cookie.domain == "prepa-epita.helvetius.net" && cookie.name == "PHPSESSID") {
+                            print("PHPSESSID Cookie: \(cookie.value)")
+
+                            self.pegasusAuthModel.setPhpSessId(newPhpSessId: cookie.value)
+                            DispatchQueue.main.async {
+                              self.isPresented = false
                             }
+                            return
                         }
                     }
                 }
-            }
-            decisionHandler(.allow)
+            })
         }
+        
     }
 
     func makeCoordinator() -> Coordinator {
